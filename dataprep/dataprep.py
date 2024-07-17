22d3e34d383f4eb51cccb41575caaacb52be2a5c
Kavitha Rajendran <karajendran@google.com>
2024-07-17 13:24:54 -0700

    renamed few variables

--- dataprep/dataprep.py
+++ dataprep/dataprep.py
@@ -10,9 +10,11 @@ from datasets import DatasetDict
 from datasets import Dataset
 import os
 
-PROJECT_ID = os.getenv('PROJECT', 'gkebatchexpce3c8dcb')
+PROJECT_ID = os.getenv('PROJECT_ID', 'gkebatchexpce3c8dcb')
 FINETUNE_DS_BUCKET = os.getenv('FINETUNE_DS_BUCKET', 'kh-finetune-ds/auto/dataset-it') # The bucket which contains the preprocessed data
 REGION = os.getenv('REGION', 'us-central1')
+PREPROCESSED_DATA_PATH = os.getenv('PREPROCESSED_DATA_PATH','gs://gkebatchexpce3c8dcb-dev-processing/flipkart_preprocessed_dataset/flipkart.csv')
+
 MODEL_ID="gemini-1.5-flash-001"
 
 generation_config = {
@@ -59,11 +61,12 @@ def filter_low_value_count_rows(df, column_name, min_count=10):
 
 
 def prep_context():
-    temp1 = pd.read_csv("gs://gkebatchexpce3c8dcb-dev-processing/flipkart_preprocessed_dataset/flipkart.csv")
+
+    preprocessed = pd.read_csv(PREPROCESSED_DATA_PATH)
     # renaming column name
-    temp1.rename(columns={'uniq_id': 'Id', 'product_name': 'Name', 'description': 'Description', 'brand': 'Brand',
+    preprocessed.rename(columns={'uniq_id': 'Id', 'product_name': 'Name', 'description': 'Description', 'brand': 'Brand',
                           'attributes': 'Specifications'}, inplace=True)
-    df = temp1[['Name', 'Description', 'Specifications', 'Brand', 'c0_name', 'c1_name', 'c2_name', 'c3_name']]
+    df = preprocessed[['Name', 'Description', 'Specifications', 'Brand', 'c0_name', 'c1_name', 'c2_name', 'c3_name']]
 
     # Filter only clothing products
     filtered_df = df[df['c0_name'] == 'Clothing']
