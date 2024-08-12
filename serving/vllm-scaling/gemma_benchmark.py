9c771471e614e68b031c054e5c2463f909a5eaa1
Kent Hua <kenthua@google.com>
2024-08-12 22:50:38 +0000

    east4 autoscale testing

---
+++ serving/vllm-scaling/gemma_benchmark.py
@@ -0,0 +1,46 @@
+from locust import FastHttpUser, task, between
+
+# gemma 1.1
+# model_id = "/model-data/hyperparam/model-job-1"
+# gemma 2
+# model_id = "/model-data/hyperparam-gemma2/model-job-1"
+model_id = "/model-data/model-gemma2-a100/experiment-a2aa2c3it1"
+# model_id = "TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ"
+message1 = (
+    "I'm looking for comfortable cycling shorts for women, what are some good options?"
+)
+message2 = "Tell me about some tops for men, looking for different styles"
+
+
+class TestUser(FastHttpUser):
+    wait_time = between(1, 5)
+
+    @task(50)
+    def test1(self):
+        self.client.post(
+            "/v1/chat/completions",
+            json={
+                "model": model_id,
+                "messages": [{"role": "user", "content": message1}],
+                "temperature": 0.5,
+                "top_k": 1.0,
+                "top_p": 1.0,
+                "max_tokens": 256,
+            },
+            name="message1",
+        )
+
+    @task(50)
+    def test2(self):
+        self.client.post(
+            "/v1/chat/completions",
+            json={
+                "model": model_id,
+                "messages": [{"role": "user", "content": message2}],
+                "temperature": 0.5,
+                "top_k": 1.0,
+                "top_p": 1.0,
+                "max_tokens": 256,
+            },
+            name="message2",
+        )
